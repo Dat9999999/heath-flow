553130d49b6a57ad9696c77b9066052f48df160f
Dat9999999 <huynhtandat184@gmail.com>
2025-11-24 15:19:00 +0700

    create login DTO

--- app/src/main/java/com/project/back_end/DTO/Login.java
+++ app/src/main/java/com/project/back_end/DTO/Login.java
@@ -1,5 +1,6 @@
 package com.project.back_end.DTO;
 
+
 public class Login {
     
 // 1. 'email' field:
@@ -7,15 +8,29 @@ public class Login {
 //    - Description:
 //      - Represents the email address used for logging into the system.
 //      - The email field is expected to contain a valid email address for user authentication purposes.
-
+    private String identifier;
 // 2. 'password' field:
 //    - Type: private String
 //    - Description:
 //      - Represents the password associated with the email address.
 //      - The password field is used for verifying the user's identity during login.
 //      - It is generally hashed before being stored and compared during authentication.
+    private String password;
+    public String getIdentifier() {
+        return identifier;
+    }
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
 
-// 3. Constructor:
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    // 3. Constructor:
 //    - No explicit constructor is defined for this class, as it relies on the default constructor provided by Java.
 //    - This class can be initialized with setters or directly via reflection, as per the application's needs.
 
